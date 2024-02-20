90800c91102facff404f51dd57ad010c8da27c29
shyam chauhan <mitsme44@gmail.com>
2024-02-20 09:04:20 +0530

    Brute force added functions modified

--- Caesar_cipher/Caesar_cipher.py
+++ Caesar_cipher/Caesar_cipher.py
@@ -1,69 +1,90 @@
 
-def encrypt():   #encryption function defination
-	flag = 0	#flag variable declaration
-	text = input("Enter Text you want to encrypt: ") #taking input of plain text from user
-	key = int(input("Enter Key you want to use for encryption (default is 3) : ") or 3)  #key that will decide how many letters needed to shift default is 3
-	length = len(text) 	#determining length of input
-	enc = '' 	#encryption text variable declaration
+def encrypt(text,key):
+	'''
+	Encrypts text using caesar cipher method
+	:param text: text to encrypt
+	:param key: key in integer to be used as shift of characters
+	:return enc: encrypted text
+	''' 
+	flag = 0
+	length = len(text) 	
+	enc = '' 	
 	
-	for i in range(0,length):   #loop until last character of plain text
-		temp = text[i]		#storing character in temp variable
-		if(temp.islower()):	#checking if character is lower case
-			enc += chr((((ord(temp) - (ord('a')-key))%26)+ord('a')))		#encryting text by adding key to it (check README for more details)
-		elif(temp.isupper()):	#checking if character is upper case
-			enc += chr((((ord(temp) - (ord('A')-key))%26)+ord('A')))		#encryting text by adding key to it (check README for more details)
-		elif(temp == " "):	#checking if character is space
-			enc += " "      #adding space to encrypted text
+	for i in range(0,length):   
+		temp = text[i]		
+		if(temp.islower()):	
+			enc += chr((((ord(temp) - (ord('a')-key))%26)+ord('a')))		
+		elif(temp.isupper()):	
+			enc += chr((((ord(temp) - (ord('A')-key))%26)+ord('A')))		
+		elif(temp == " "):	
+			enc += " "      
 		else:
-			print("Error!!") 	#other than character(e.g. digits or special characters) encountered & printing error message
-			flag = 1		#setting up flag to 1
-			break			#breaking the loop
+			print("Error!!") 	
+			flag = 1		
+			break			
 
-	if(flag == 0):		#checking if error occured or not 
-		print("Encrypted Text is : ",enc)	#no errors so printing the encrypted text
-	else:	#error encountered
-		pass 	#error message is alrady printed so do nothing
-	
+	if(flag == 0):		
+		return enc
+	else:	
+		pass 
 
-
-def decrypt():		#decryption function defination
-	flag = 0		#flag variable declaration
-	text = input("Enter Text you want to decrypt: ")	#taking input of cypher text from user
-	key = int(input("Enter Key you want to use for decryption default is 3 : ")or 3)	#key that will decide how many letters needed to shift default is 3
-	length = len(text)	#determining length of input
-	dec = ''		#decryption text variable declaration
-
-	for i in range(0,length):	#loop until last character of plain text
-		temp = text[i]		#storing character in temp variable
-		if(temp.islower()):		#checking if character is lower case
-			dec += chr((((ord(temp) - (ord('a')+key))%26)+ord('a'))) 	#decryting text by subtracting key from it (check README for more details)
-		elif(temp.isupper()):		#checking if character is upper case
-			dec += chr((((ord(temp) - (ord('A')+key))%26)+ord('A')))		#decryting text by subtracting key from it (check README for more details)
-		elif(temp == " "):		#checking if character is space
-			dec += " "		#adding space to decrypted text
+def decrypt(cipher,key):
+	'''
+	Decrypts cipher using caesar cipher method
+	:param cipher: cipher to decrypt
+	:param key: key in integer to be used as shift of characters
+	:return enc: encrypted text
+	''' 		
+	flag = 0		
+	length = len(cipher)	
+	dec = ''		
+	for i in range(0,length):	
+		temp = cipher[i]		
+		if(temp.islower()):		
+			dec += chr((((ord(temp) - (ord('a')+key))%26)+ord('a'))) 	
+		elif(temp.isupper()):		
+			dec += chr((((ord(temp) - (ord('A')+key))%26)+ord('A')))		
+		elif(temp == " "):		
+			dec += " "		
 		else:
-			print("Error!!")	#other than character(e.g. digits or special characters) encountered & printing error message
-			flag = 1		#setting up flag to 1
-			break			#breaking the loop
-
-	if(flag == 0):		#checking if error occured or not 
-		print("decrypted Text is : ",dec)		#no errors so printing the decrypted text
-	else:			#error encountered
-		pass		#error message is alrrady printed so do nothing
+			print("Error!!")	
+			flag = 1		
+			break			
+	if(flag == 0):		
+		return dec		
+	else:			
+		pass		
 	
+def brute_caesar(cipher):
+	'''
+	Brute force attack on caesar cipher
+	:param cipher: cipher to attack
+	'''
+	for i in range(1,26):
+		print(f"With key :{i} Text is :{decrypt(cipher,i)}")
 
 
-if __name__ == '__main__':	#checking if program is running as main program
-	choice = 1	#initializing choice variable
-	while(choice != 3):		#running loop until user chooses to exit by presssing 3
-		choice = int(input("1. For encryption \n2. For decryption \n3. For exit\n Enter your Choice :"))	#user input for choice where 1 is encryption 2 is decryption and 3 is exit
-		if(choice == 1):	#choice 1
-			encrypt()	#function call to encrypt
-		elif(choice == 2):	#choice 2
-			decrypt()	#function call to decrypt
-		elif(choice == 3):	#choice 3
-			print("You choose to exit")	#displaying exit message
-			exit(0)		#exiting with no errors
-		else:		#unknown choice
-			print("Wrong choice !")		#displaying wrong choice message and running loop again
-	
+if __name__ == '__main__':	
+	'''
+	Main method
+	'''
+	choice = 1	
+	while(choice !=4):		
+		choice = int(input("1. For encryption \n2. For decryption \n3. For Brute force \n. For exit\n Enter your Choice :"))	
+		if(choice == 1):
+			text = input("Enter Text you want to encrypt: ") 
+			key = int(input("Enter Key you want to use for encryption (default is 3) : ") or 3)  
+			print("Encrypted text is : ",encrypt(text,key))
+		elif(choice == 2):	
+			cipher = input("Enter cipher you want to decrypt: ")	
+			key = int(input("Enter Key you want to use for decryption default is 3 : ")or 3)	
+			decrypt(cipher,key)	
+			print("Decrypted text is : ",decrypt(cipher,key))
+		elif(choice == 3):	
+			cipher = input("Enter cipher you want to decrypt: ")	
+			brute_caesar(cipher)
+		elif(choice == 4):	
+			print("You choose to exit")	
+			exit(0)		
+		else:		
+			print("Wrong choice !")	
